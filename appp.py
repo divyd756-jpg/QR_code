f9f12490945d1ccbad205f691dd75fa5daf67774
divyd756-jpg <divyd756@gmail.com>
2026-05-06 13:21:12 +0530

    Create appp.py

---
+++ appp.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import qrcode
+from PIL import Image
+
+#page title
+st.title("Qr code generator")
+data=st.text_input("enter url")
+if st.button("Generate QR"):
+  if data:
+    qr = qrcode.make(data)
+    qr.save("gr.png")
+    
+    img = Image.open("qr.png")
+    st.image(img, caption="Generated QR Code")
+
+    
+    with open("qr.png","rb") as f:
+       st.download_button("Download QR",f,file_name="qr.png")
+  else:
+    st.warning("please enter some text")
+              
+
+
+
